a353b6d38518084cfd1ee2346a995b4d836d3aae
T.DHINESH <145428367+Dinesh-TSD@users.noreply.github.com>
2023-11-19 18:57:02 +0530

    shop

--- src/Navbar/Search.jsx
+++ src/Navbar/Search.jsx
@@ -7,7 +7,7 @@ const Search = () => {
         <>
             <nav className="navbar navbar-light bg-light mt-5">
                 <div className="container">
-                    <h4 className="navbar-brand">Navbar</h4>
+                    <h4 className="navbar-brand">Best Offers On Mobiles</h4>
                     <form className="d-flex">
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                         <button className="btn btn-outline-success" type="submit">Search</button>
